0ea3f0b35f8e21b19a454386b78239d037f64e65
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2024-01-17 21:39:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,117 +1,45 @@
 #include "monty.h"
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - entry into interpreter
- * @argc: argc counter
- * @argv: arguments
- * Return: 0 on success
- */
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
-	int fd;
-	char *buffer;
-	stack_t *h = NULL;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	fd = open_file(argv[1]);
-	buffer = read_file(fd);
-	tokenize(buffer, &h);
-	free_resources(fd, buffer, &h);
-	return (0);
-}
-
-/**
- * open_file - Opens a file and returns the file descriptor.
- * @filename: The name of the file to open.
- * Return: The file descriptor on success, exits on failure.
- */
-int open_file(const char *filename)
-{
-	int fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	return (fd);
-}
-
-/**
- * read_file - Reads content from a file into a buffer.
- * @fd: The file descriptor to read from.
- * Return: A pointer to the allocated buffer containing file content,
- *         exits on failure.
- */
-char *read_file(int fd)
-{
-	char *buffer = malloc(sizeof(char) * 10000);
-	ssize_t n_read;
-
-	if (!buffer)
-		exit(EXIT_FAILURE);
-	n_read = read(fd, buffer, 10000);
-
-	if (n_read == -1)
+	while (read_line > 0)
 	{
-		free(buffer);
-		close(fd);
-		exit(EXIT_FAILURE);
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
 	}
-	return (buffer);
-}
-
-/**
- * tokenize - Tokenizes the input buffer and processes each token.
- * @buffer: The buffer containing the input to tokenize and process.
- * @h: A pointer to the stack used in processing.
- */
-void tokenize(char *buffer, stack_t **h)
-{
-	char *token = strtok(buffer, "\n\t\a\r ;:");
-	unsigned int line = 1;
-	int ispush = 0;
-
-	while (token != NULL)
-	{
-		if (ispush == 1)
-		{
-			push(h, line, token);
-			ispush = 0;
-		}
-		else if (strcmp(token, "push") == 0)
-		{
-			ispush = 1;
-		}
-		else
-		{
-			if (get_op(token) != 0)
-				get_op(token)(h, line);
-			else
-			{
-				free_dlist(h);
-				fprintf(stderr, "L%d: unknown instruction %s\n", line, token);
-				exit(EXIT_FAILURE);
-			}
-		}
-	line++;
-	token = strtok(NULL, "\n\t\a\r ;:");
-	}
-}
-
-/**
- * free_resources - Frees resources (memory and file descriptor).
- * @fd: The file descriptor to close.
- * @buffer: The buffer to free.
- * @h: A pointer to the stack to free.
- */
-void free_resources(int fd, char *buffer, stack_t **h)
-{
-	free_dlist(h);
-	free(buffer);
-	close(fd);
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
