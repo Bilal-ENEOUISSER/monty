11ce8978fab17fb6b439e4521754ab527246253c
mohsine29 <125808498+mohsine29@users.noreply.github.com>
2024-01-17 13:03:51 -0800

    update

--- main.c
+++ main.c
@@ -30,16 +30,16 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line = get_op(&content, &size, file);
+		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
 		{
-			execute(content, &stack, counter, file);
+			get_op(content, &stack, counter, file);
 		}
 		free(content);
 	}
-	free_stack(stack);
+	free_dlist(stack);
 	fclose(file);
 return (0);
 }
