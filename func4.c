911c8e87050b925e708acf805dce29a10d6f142c
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2024-01-18 00:37:26 +0100

    Create func4.c

---
+++ func4.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * stack_ - prints the top
+ * @h: stack h
+ * @counter: line_number
+ * Return: no return
+*/
+void stack_(stack_t **h, unsigned int counter)
+{
+	(void)h;
+	(void)counter;
+	bus.lifi = 0;
+}
