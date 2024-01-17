edec0a8dd71154cc9d99cc727b8a2b4169c878b1
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2024-01-17 23:40:59 +0100

    Update func3.c

--- func3.c
+++ func3.c
@@ -36,21 +36,21 @@ void addnode_(stack_t **h, int n)
 int execute_(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
-				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
-				{"pop", f_pop},
-				{"swap", f_swap},
-				{"add", f_add},
-				{"nop", f_nop},
-				{"sub", f_sub},
-				{"div", f_div},
-				{"mul", f_mul},
-				{"mod", f_mod},
-				{"pchar", f_pchar},
-				{"pstr", f_pstr},
-				{"rotl", f_rotl},
-				{"rotr", f_rotr},
-				{"queue", f_queue},
-				{"stack", f_stack},
+				{"push", push_}, {"pall", pall_}, {"pint", pint_},
+				{"pop", pop_},
+				{"swap", swap_},
+				{"add", add_},
+				{"nop", nop_},
+				{"sub", sub_},
+				{"div", div_},
+				{"mul", mul_},
+				{"mod", mod_},
+				{"pchar", pchar_},
+				{"pstr", pstr_},
+				{"rotl", rotl_},
+				{"rotr", rotr_},
+				{"queue", queue_},
+				{"stack", stack_},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
