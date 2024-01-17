50d766f9b6e77f95e4b3dbdb979996ffa5d07e7e
Bilal ENEOUISSER <77547073+Bilal-ENEOUISSER@users.noreply.github.com>
2024-01-18 00:36:42 +0100

    Update func3.c

--- func3.c
+++ func3.c
@@ -148,16 +148,3 @@ void addqueue_(stack_t **h, int n)
 }
 
 
-
-/**
- * stack_ - prints the top
- * @h: stack h
- * @counter: line_number
- * Return: no return
-*/
-void stack_(stack_t **h, unsigned int counter)
-{
-	(void)h;
-	(void)counter;
-	bus.lifi = 0;
-}
